26f4ce1b879f35085503cd44698c22d8b2e21a2e
ann88412a <ann88412a@gmail.com>
2024-02-27 16:41:53 +0800

    add new lesson plan

---
+++ lesson_plan_two/js/feedback.js
@@ -0,0 +1,328 @@
+// 制定回饋規則 打分機制
+
+function feedback(){
+    console.log(syringe_value);
+    var wrong_syringe = 0;
+    for (const [key, value] of Object.entries(syringe_value)) {
+        if (value[0] != 0){
+            wrong_syringe = wrong_syringe + 1;
+        }
+      }
+
+    var score = 0;
+    
+    var id_name = 'ID：' + document.getElementById('IDF_ID').value + ' 姓名：' + document.getElementById('IDF_name').value;
+    $('.ODF_ID')[0].innerText= id_name;
+
+    // 1
+    const radios = document.getElementsByName('barcode');
+    var img1 = document.getElementById('1 img');
+    paitent_r = ''
+    if (radios[1].checked){
+        cognition.push(1);
+        score = score + 1;
+        img1.src="pic/ok_w.png";
+        // paitent_r = '掃描結果：' + $('.ODF_value')[0].innerText + ',   您判斷是否正確： Yes';
+        correctness.push(1);
+    }else{
+        cognition.push(0);
+        img1.src="pic/wrong_w.png";
+        paitent_r = '掃描結果：' + $('.ODF_value')[0].innerText + ',   您判斷是否正確： No';
+        correctness.push(0);
+    }
+    
+    document.getElementById('paitent r').innerHTML = paitent_r;
+
+
+    // 2
+    var img2 = document.getElementById('2 img');
+    if (!document.getElementById('check1').checked){
+        r2r = '您不給 Amiodarone(Cordarone) 150mg/3ml/amp 的理由：' + document.getElementById('Amiodarone(Cordarone) 150mg/3ml/amp r no').value;
+        reason.push(document.getElementById('Amiodarone(Cordarone) 150mg/3ml/amp r no').value);
+        cognition.push(1);
+        if (1==1){ 
+            score = score + 1;
+            img2.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img2.src="pic/wrong_w.png";
+            r2r = r2r + '\n -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('2 r 2').innerHTML = r2r;
+
+    }else{
+        cognition.push(0);
+        img2.src="pic/wrong_w.png";
+        r2 = '您給 Amiodarone(Cordarone) 150mg/3ml/amp 的理由：' + document.getElementById('Amiodarone(Cordarone) 150mg/3ml/amp r').value;
+        document.getElementById('2 r').innerHTML = r2;
+        correctness.push(0);
+        reason.push(document.getElementById('Amiodarone(Cordarone) 150mg/3ml/amp r').value);
+    
+    }
+    
+    // 3
+    var img3 = document.getElementById('3 img');
+    r3r = ''
+    if (!document.getElementById('check6').checked){
+        // r3r = '您不給 Plavix (Clopidogrel) 75mg/tab 的理由：' + document.getElementById('Plavix (Clopidogrel) 75mg/tab r no').value;
+        reason.push(document.getElementById('Plavix (Clopidogrel) 75mg/tab r no').value);
+        cognition.push(1); // 實際藥物需要300mg 但實際藥丸只有225mg
+        if (pill_detect['Clopidogrel'] == 0){
+            score = score + 1;
+            img3.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img3.src="pic/wrong_w.png";
+            // r3r = r3r + '\n -> 答錯原因：實際給藥錯誤';
+            r3r = ' -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('3 r 3').innerHTML = r3r;
+
+    }else{
+        cognition.push(0);
+        img3.src="pic/wrong_w.png";
+        r3 = '您給 Plavix (Clopidogrel) 75mg/tab 的理由：' + document.getElementById('Plavix (Clopidogrel) 75mg/tab r').value;
+        document.getElementById('3 r').innerHTML = r3;
+        r3 = r3 + '\n -> 答錯原因：MAR單認知錯誤';
+        correctness.push(0);
+        reason.push(document.getElementById('Plavix (Clopidogrel) 75mg/tab r').value);
+    }
+
+
+    // 4
+    var img4 = document.getElementById('4 img');
+    if (!document.getElementById('check2').checked){
+        r4r = '您不給 KCL (Potassium chloride) 20mEq/10mL/amp 的理由：' + document.getElementById('KCL (Potassium chloride) 20mEq/10mL/amp r no').value;
+        reason.push(document.getElementById('KCL (Potassium chloride) 20mEq/10mL/amp r no').value);
+        cognition.push(1);
+        if (1==1){ 
+            score = score + 1;
+            img4.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img4.src="pic/wrong_w.png";
+            r4r = r4r + '\n -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('4 r 4').innerHTML = r4r;
+
+    }else{
+        cognition.push(0);
+        img4.src="pic/wrong_w.png";
+        r4 = '您給 KCL (Potassium chloride) 20mEq/10mL/amp 的理由：' + document.getElementById('KCL (Potassium chloride) 20mEq/10mL/amp r').value;
+        document.getElementById('4 r').innerHTML = r4;
+        correctness.push(0);
+        reason.push(document.getElementById('KCL (Potassium chloride) 20mEq/10mL/amp r').value);
+    
+    }
+
+
+    // 5
+    var img5 = document.getElementById('5 img');
+    
+    if (!document.getElementById('check4').checked){
+        r5r = '您不給 Rolikan (Sodium bicarbonate) 7% 20mL/amp 的理由：' + document.getElementById('Rolikan (Sodium bicarbonate) 7% 20mL/amp r no').value;
+        reason.push(document.getElementById('Rolikan (Sodium bicarbonate) 7% 20mL/amp r no').value);
+        cognition.push(1);
+        if (1==1){ 
+            score = score + 1;
+            img5.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img5.src="pic/wrong_w.png";
+            r5r = r5r + '\n -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('5 r 5').innerHTML = r5r;
+
+    }else{
+        cognition.push(0);
+        img5.src="pic/wrong_w.png";
+        r5 = '您給 Rolikan (Sodium bicarbonate) 7% 20mL/amp 的理由：' + document.getElementById('Rolikan (Sodium bicarbonate) 7% 20mL/amp r').value;
+        document.getElementById('5 r').innerHTML = r5;
+        correctness.push(0);
+        reason.push(document.getElementById('Rolikan (Sodium bicarbonate) 7% 20mL/amp r').value);
+    
+    }
+
+
+    // 6
+    var img6 = document.getElementById('6 img');
+    
+    if (!document.getElementById('check3').checked){
+        r6r = '您不給 Cefazolin 1000mg/vail 的理由：' + document.getElementById('Cefazolin 1000mg/vail r no').value;
+        reason.push(document.getElementById('Cefazolin 1000mg/vail r no').value);
+        cognition.push(1);
+        if (1==1){ 
+            score = score + 1;
+            img6.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img6.src="pic/wrong_w.png";
+            r6r = r6r + '\n -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('6 r 6').innerHTML = r6r;
+
+    }else{
+        cognition.push(0);
+        img6.src="pic/wrong_w.png";
+        r6 = '您給 Cefazolin 1000mg/vail 的理由：' + document.getElementById('Cefazolin 1000mg/vail r').value;
+        document.getElementById('6 r').innerHTML = r6;
+        correctness.push(0);
+        reason.push(document.getElementById('Cefazolin 1000mg/vail r').value);
+    
+    }
+
+
+    // 7 
+    var img7 = document.getElementById('7 img');
+    r7r = ''
+    if (!document.getElementById('check7').checked){
+        // r7r = '您不給 Aspirin 100mg/tab 的理由：' + document.getElementById('Aspirin 100mg/tab r no').value;
+        reason.push(document.getElementById('Aspirin 100mg/tab r no').value);
+        cognition.push(1);
+        if (pill_detect['Aspirin'] == 0){
+            score = score + 1;
+            img7.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img7.src="pic/wrong_w.png";
+            // r7r = r7r + '\n -> 答錯原因：實際給藥錯誤';
+            r7r = ' -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+        }
+
+        document.getElementById('7 r 7').innerHTML = r7r;
+    }else{
+        cognition.push(0);
+        img7.src="pic/wrong_w.png";
+        r7 = '您給 Aspirin 100mg/tab 的理由：' + document.getElementById('Aspirin 100mg/tab r').value;
+        document.getElementById('7 r').innerHTML = r7;
+        r7 = r7 + '\n -> 答錯原因：MAR單認知錯誤';
+        correctness.push(0);
+        reason.push(document.getElementById('Aspirin 100mg/tab r').value);
+    }
+
+    // 8
+    var img8 = document.getElementById('8 img');
+    r8 = ''
+    if (document.getElementById('check8').checked){
+        // r8 = '您給 Tulip （Atorvastatin）20mg/tab 的理由：' + document.getElementById('Tulip （Atorvastatin）20mg/tab r').value;
+        reason.push(document.getElementById('Tulip （Atorvastatin）20mg/tab r').value);
+        cognition.push(1);
+        if (pill_detect['Tulip'] == 1){
+            score = score + 1;
+            img8.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img8.src="pic/wrong_w.png";
+            // r8 = r8 + '\n -> 答錯原因：實際給藥錯誤';
+            r8 = ' -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+        }
+
+        document.getElementById('8 r').innerHTML = r8;
+    }else{
+        cognition.push(0);
+        img8.src="pic/wrong_w.png";
+        r8r = '您不給 Tulip （Atorvastatin）20mg/tab 的理由：' + document.getElementById('Tulip （Atorvastatin）20mg/tab r').value;
+        document.getElementById('8 r 8').innerHTML = r8r;
+        r8r = r8r + '\n -> 答錯原因：MAR單認知錯誤'
+        correctness.push(0);
+        reason.push(document.getElementById('Tulip （Atorvastatin）20mg/tab r no').value);
+    }
+
+
+    // 9
+    var img9 = document.getElementById('9 img');
+    
+    if (document.getElementById('check5').checked){
+        r9r = '您給 Heparin 25000units/vail 的理由：' + document.getElementById('Heparin 25000units/vail r').value;
+        reason.push(document.getElementById('Heparin 25000units/vail r').value);
+        cognition.push(1);
+        if (medicines['Heparin 25000units/vail']['verification']=='4710031297121' && 0.75<=medicines['Heparin 25000units/vail']['injection'] && medicines['Heparin 25000units/vail']['injection']<=0.85 && medicines['Heparin 25000units/vail']['way'][0]=='IVP' && medicines['Heparin 25000units/vail']['dilution']=="0"){ 
+            score = score + 1;
+            img9.src="pic/ok_w.png";
+            correctness.push(1);
+
+        }else{
+            img9.src="pic/wrong_w.png";
+            r9r = r9r + '\n -> 答錯原因：實際給藥錯誤';
+            correctness.push(0);
+
+        }
+        document.getElementById('9 r 9').innerHTML = r9r;
+        console.log(medicines['Heparin 25000units/vail']['verification']!=null);
+        console.log(0.75<=medicines['Heparin 25000units/vail']['injection']<=0.85);
+        console.log(medicines['Heparin 25000units/vail']['way']==['IVP']);
+        console.log(medicines['Heparin 25000units/vail']['dilution']=="0");
+
+    }else{
+        cognition.push(0);
+        img9.src="pic/wrong_w.png";
+        r9 = '您不給 Heparin 25000units/vail 的理由：' + document.getElementById('Heparin 25000units/vail r no').value;
+        document.getElementById('9 r').innerHTML = r9;
+        correctness.push(0);
+        reason.push(document.getElementById('Heparin 25000units/vail r no').value);
+    
+    }
+
+
+    // 10
+    // var img10 = document.getElementById('10 img');
+    
+    // if (!document.getElementById('check9').checked){
+    //     r10r = '您不給 FLU-D (Fluconazole) 50mg/tab 的理由：' + document.getElementById('FLU-D (Fluconazole) 50mg/tab r no').value;
+    //     reason.push(document.getElementById('FLU-D (Fluconazole) 50mg/tab r no').value);
+    //     cognition.push(1);
+    //     if (pill_detect['FLU'] == 0){
+    //         score = score + 1;
+    //         img10.src="pic/ok_w.png";
+    //         correctness.push(1);
+
+    //     }else{
+    //         img10.src="pic/wrong_w.png";
+    //         r10r = r10r + '\n -> 答錯原因：實際給藥錯誤';
+    //         correctness.push(0);
+    //     }
+
+    //     document.getElementById('10 r 10').innerHTML = r10r;
+    // }else{
+    //     cognition.push(0);
+    //     img10.src="pic/wrong_w.png";
+    //     r10 = '您給 FLU-D (Fluconazole) 50mg/tab 的理由：' + document.getElementById('FLU-D (Fluconazole) 50mg/tab r').value;
+    //     document.getElementById('10 r').innerHTML = r10;
+    //     correctness.push(1);
+    //     reason.push(document.getElementById('FLU-D (Fluconazole) 50mg/tab r').value);
+    // }
+
+    
+    // score
+    document.getElementById('score').innerHTML = score;
+    if (score >= 7)
+    {
+        document.getElementById('review').innerHTML = '高等';
+    }else if(score >= 4)
+    {
+        document.getElementById('review').innerHTML = '中等';
+    }else
+    {
+        document.getElementById('review').innerHTML = '低等';
+    }
+
+}   
---
+++ lesson_plan_two/js/ida.js
@@ -0,0 +1,188 @@
+// 接收 IoTtalk 資訊處理
+
+var output_patient_barcode_bt = 0;
+var output_pill_bt = 0;
+var pill_detect = { 'Aspirin': -1,
+                    'Tulip': -1,
+                    'Clopidogrel': -1,};
+
+var syringe_value = {"AMIKACIN INJECTION 250MG/ML 'TAI YU'": -1,
+                        "AMPOLIN INJECTION 500MG": -1,
+                        "CEFAZOLIN INJECTION 1GM 'C.C.P.'": -1,
+                        "CLEXANE INJECTION": -1,
+                        "CORDARONE INJECTION": -1,
+                        "Heparin Sodium Injection 5000 IU/ml 'Tai Yu'": -1,
+                        "MILLISROL INJECTION": -1,
+                        "Oxacillin Powder for Injection 'CYH'": -1,
+                        "Progesterone Injection 'Chi Sheng'": -1,
+                        "ROLIKAN INJECTION (SODIUM BICARBONATE)": -1,
+                        "SODIUM BICARBONATE INJECTION 'CHI SHENG'": -1,
+                        "Sirolac IV Injection 30 mg/ml 'ASTAR'": -1,};
+var patient_barcode = '1234567890AAA'
+var cognition = []
+
+var client_uid = (Math.random() + 1).toString(36).substring(7) + (Math.random() + Math.random()).toString(36).substring(8);
+console.log(client_uid);
+
+
+
+
+$(function(){
+        csmapi.set_endpoint ('https://class.iottalk.tw');
+        var profile = {
+		    'dm_name': 'Medication',          
+			'idf_list':[Barcode_I, Pill_Detect_I, Syringe_I],
+			'odf_list':[Barcode_Result_O, Pill_Detect_Result_O, Syringe_Result_O],
+		    'd_name': 'PlatformPlan1',
+		    // 'd_name': 'Platform_Demo_anna',
+        };
+
+		// idf
+        function Barcode_I(data){
+            // $('.ODF_value')[0].innerText=data[0];
+         }
+
+        function Pill_Detect_I(data){
+            // $('.ODF_value')[0].innerText=data[0];
+         }
+
+        function Syringe_I(data){
+        // $('.ODF_value')[0].innerText=data[0];
+        }
+
+        // odf
+        function Barcode_Result_O(data){
+            console.log('Barcode_Result_O', data);
+            if (data[0] == client_uid){
+                var data_value = JSON.parse(data[1]);
+                console.log(data_value);
+                console.log(data_value['barcode']);
+                
+                if (data_value['medicine_info']){
+                    data_value = JSON.parse(data[1]);
+                    // console.log("data_value is:", data_value['medicine_info']);
+                    // console.log(document.getElementsByName("verification_button_id").value);
+                    medicines[medicine_keys[document.getElementsByName("verification_button_id").value]]['verification'] = data_value['medicine_info'];
+                    createtbl(); 
+                    
+                    // console.log(data_value['medicine_info'][0]);
+                    JumpToPage(5);
+                }
+                else{
+                    $.getJSON(domain_name_url + '/api/_patient', {
+                    barcode: data_value['barcode']
+                    }, function(data) {
+                    console.log(data);
+                    var resp = data
+                    $('.ODF_value')[0].innerText = resp['info'];
+                    var img = document.getElementById('barcode_scanner');
+                    img.src="pic/ok1.jpeg";
+                    $('.patient_barcode_hint')[0].innerText = "★ 辨識完成請繼續執行下一步＾＿＾";
+                    });
+                }
+                
+                // if (data_value['patient_info']){
+                //     $('.ODF_value')[0].innerText = data_value['patient_info'];
+                //     var img = document.getElementById('barcode_scanner');
+                //     img.src="pic/ok1.jpeg";
+                //     $('.patient_barcode_hint')[0].innerText = "★ 辨識完成請繼續執行下一步＾＿＾";
+                // }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                    
+                // else if (data_value['medicine_info']){
+                //     data_value = JSON.parse(data[1]);
+                //     medicines[medicine_keys[document.getElementsByName("verification_button_id").value]]['verification'] = data_value['medicine_info'][0];
+                //     createtbl(); 
+                //     JumpToPage(0); 
+                // }
+            }
+            
+        }
+
+        function Pill_Detect_Result_O(data){
+            
+            if (output_pill_bt > 0 && data[0] == client_uid){
+                    var img = document.getElementById('pill_odf');
+                    img.src="pic/ok1.jpeg";
+
+                    pill_detect['Aspirin'] = data[1];
+                    // pill_detect['none5'] = data[8];
+                    pill_detect['Tulip'] = data[2];
+                    // pill_detect['none6'] = data[9];
+                    pill_detect['Clopidogrel'] = data[3];
+                    // pill_detect['none1'] = data[4];
+                    // pill_detect['none2'] = data[5];
+                    // pill_detect['none3'] = data[6];
+                    // pill_detect['none4'] = data[7];
+                    $('.pill_hint')[0].innerText = '★ 辨識完成請繼續執行下一步＾＿＾';
+
+                    syringe_block = document.getElementById('syringe block');
+                    syringe_block.style.display = "block";
+
+                }
+            
+        }
+
+        function Syringe_Result_O(data){
+            console.log('Syringe_Result_O', data);
+            console.log('Syringe_Result_O', data[2]);
+            if(data[0] == client_uid){
+                var Syringe_number = parseInt(medicines[medicine_keys[document.getElementsByName("injection_button_id").value]]['syringe_num']);
+                // medicines[medicine_keys[document.getElementsByName("injection_button_id").value]]['injection'] = data[2];
+
+                if(Syringe_number==2){
+                    var button = document.getElementById('recognition_2');
+                    if(button.value=='unclicked'){
+                        medicines[medicine_keys[document.getElementsByName("injection_button_id").value]]['injection'] = data[2];
+                        createtbl();
+                        enableButton('recognition_1');
+                        var fontElement1 = document.getElementById("show_syringe_volume1");
+                        fontElement1.innerHTML = "第1支針 - 辨識數值 : " + data[2].toString() + " ml";
+                        //顯示第二支針劑的文字和按鈕
+                        var button = document.getElementById('recognition_2');
+                        button.style.display = "block";
+                        var fontElement2 = document.getElementById("show_syringe_volume2");
+                        fontElement2.style.display = "block";
+                        console.log("第1支針辨識完成");
+                        // console.log(data);
+                    }
+                    else{
+                        medicines[medicine_keys[document.getElementsByName("injection_button_id").value]]['injection'] += data[2];
+                        enableButton('recognition_2');
+                        createtbl();
+                        var fontElement = document.getElementById("show_syringe_volume2");
+                        fontElement.innerHTML = "第2支針 - 辨識數值 : " + data[2].toString() + " ml";
+                        enableButton("next_step");
+                        clickCheck('recognition_1');
+                        clickCheck('recognition_2');
+                        console.log("第2支針辨識完成");
+                        // console.log(data);
+                    }
+                }
+                else{
+                    medicines[medicine_keys[document.getElementsByName("injection_button_id").value]]['injection'] = data[2];
+                    enableButton('recognition_1');
+                    var fontElement = document.getElementById("show_syringe_volume1");
+                    fontElement.innerHTML = "針劑辨識數值 : " + data[2].toString() + " ml";
+                    createtbl();
+                    var button = document.getElementById("next_step");
+                    button.disabled = false;
+                    console.log("辨識完成");
+                }
+            }
+        }
+
+        
+
+
+
+        
+      
+/*******************************************************************/                
+        function ida_init(){
+	    console.log(profile.d_name);
+	}
+        var ida = {
+            'ida_init': ida_init,
+        }; 
+        dai(profile,ida);     
+});

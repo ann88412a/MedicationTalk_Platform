78964999f8041aeae2dd394c5c6c166e97ed1215
lazyho <lazyho8809@gmail.com>
2024-04-29 22:49:12 +0800

    choose machine

--- lesson_plan_three/js/update.js
+++ lesson_plan_three/js/update.js
@@ -134,19 +134,24 @@ for (let i=0; i<10; i++){
 
 var correctness = [];
 var reason = [];
+var machine_ID = "";
 
 
 function check_page(n){
     if (n === 1){
+        //選擇機器
+        var selectElement = document.getElementById("IDF_machine");
+        console.log(selectElement.value);
+        machine_ID = selectElement.value;
+
+
         const idf_id = document.getElementById('IDF_ID');
         const idf_name = document.getElementById('IDF_name');
         const idf_machine = document.getElementById('IDF_machine');
         if (idf_id.value != "" && idf_name.value != "" && idf_machine.value != ""){
+        // if (idf_id.value != "" && idf_name.value != ""){
             plusSlides(1);
             window.alert("您即將進入給藥情境\n [請依照指示操作]");
-            client_uid = idf_machine.value + client_uid;
-            console.log('client_uid');
-            console.log(client_uid);
 
             // set syringe URL 
             subFeature = document.getElementById('iframe_syringe')
@@ -345,18 +350,18 @@ function check_page(n){
 function check_bt(f){
     if (f === 'barcode'){
         $('.patient_barcode_hint')[0].innerText = '請到Barcode機的螢幕上操作';
-        dan.push('Barcode-I', [client_uid, 'Device_Demo', 'patient', true]);
-        dan.push('Lesson_Plan-I', 3);
+        dan.push('Barcode-I', [client_uid, machine_ID, 'patient', true]);
+        dan.push('Lesson_Plan-I', [client_uid, machine_ID, 3]);
     
 
         
         output_patient_barcode_bt = output_patient_barcode_bt + 1;
     }
     else if(f === 'medicine_info'){
-        dan.push('Barcode-I', [client_uid, 'Device_Demo', 'syringe', true]);
+        dan.push('Barcode-I', [client_uid, machine_ID, 'syringe', true]);
     }
     else if(f === 'pill'){
-        dan.push('Pill_Detect-I', [client_uid, 'Device_Demo', true]);
+        dan.push('Pill_Detect-I', [client_uid, machine_ID, true]);
         output_pill_bt = output_pill_bt + 1;
         $('.pill_hint')[0].innerText = 'waiting...我在這!! 我有近來';
     }

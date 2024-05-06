6cfb29445338bce298bae1883bf7ed71d1349fb5
YENC0810 <chouyen0810@gmail.com>
2024-05-06 22:26:57 +0800

    fix ida bug

--- lesson_plan_two/js/ida.js
+++ lesson_plan_two/js/ida.js
@@ -37,8 +37,8 @@ $(function(){
         csmapi.set_endpoint ('https://class.iottalk.tw');
         var profile = {
 		    'dm_name': 'Medication',          
-			  'idf_list':[Barcode_I, Pill_Detect_I, Syringe_I, Lesson_Plan_I],
-		  	'odf_list':[Barcode_Result_O, Pill_Detect_Result_O, Syringe_Result_O],
+            'idf_list':[Barcode_I, Pill_Detect_I, Classification_I, Volume_I, Lesson_Plan_I],
+            'odf_list':[Barcode_Result_O, Pill_Detect_Result_O, Classification_Result_O, Volume_Result_O],
 		    'd_name': 'Platform',
 		    // 'd_name': 'Platform_Demo_anna',
         };
